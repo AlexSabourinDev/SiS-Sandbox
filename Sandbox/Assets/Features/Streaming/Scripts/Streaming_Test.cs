2a15cb3244f79ad4c1ed16fa66f6dbd2a659e49e
Alex <AlexSabourinDev@gmail.com>
2019-05-07 19:51:54 -0400

    MOD: Introduced a streaming test script that loads and unloads scripts. It can be a good baseline to see how to request these things to be done.

---
+++ Sandbox/Assets/Features/Streaming/Scripts/Streaming_Test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Unity;
+using Unity.Entities;
+using Unity.Scenes;
+
+public class Streaming_Test : MonoBehaviour
+{
+    [SerializeField]
+    private SubScene m_TestScene;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            EntityManager entityManager = World.Active.EntityManager;
+            foreach(Entity entity in m_TestScene._SceneEntities)
+            {
+                entityManager.AddComponentData(entity, new RequestSceneLoaded());
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.U))
+        {
+            EntityManager entityManager = World.Active.EntityManager;
+            foreach(Entity entity in m_TestScene._SceneEntities)
+            {
+                entityManager.RemoveComponent<RequestSceneLoaded>(entity);
+            }
+        }
+    }
+}
