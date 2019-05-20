47802231d9131214b116c5586a07a832bf95a656
NathanSaidas <nathanhanlan@gmail.com>
2019-05-20 19:10:07 -0400

    ADD: Adding missing file NetConnection

---
+++ Packages/Network/Assets/Scripts/NetConnection.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace Game.Networking
+{
+    public class NetConnection : INetConnection
+    {
+        private IPEndPoint  m_EndPoint = null;
+        private string      m_Identifier = "";
+        private byte[]      m_UID = null;
+
+        public IPEndPoint EndPoint { get { return m_EndPoint; } }
+        public string Identifier { get { return m_Identifier; } }
+        public byte[] UID { get { return m_UID; } }
+
+        public NetConnection()
+        {
+
+        }
+
+        public NetConnection(IPEndPoint endPoint, string identifier, byte[] uid)
+        {
+            m_EndPoint = endPoint;
+            m_Identifier = identifier;
+            m_UID = uid;
+        }
+    }
+}
