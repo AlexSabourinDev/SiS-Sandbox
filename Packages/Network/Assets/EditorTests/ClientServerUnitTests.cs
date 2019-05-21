aa35e5fa74b29879b6d2f947263b09f91d836222
NathanSaidas <nathanhanlan@gmail.com>
2019-05-20 21:21:13 -0400

    ADD: Adding a basic unit test for Client/Server testing using a Virtual Network (nothing important in it yet)

---
+++ Packages/Network/Assets/EditorTests/ClientServerUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace Game.Networking
+{
+    public class ClientServerUnitTests
+    {
+        [Test]
+        public void BasicTest()
+        {
+            VirtualNetwork network = new VirtualNetwork();
+
+            VirtualNetServer server = new VirtualNetServer();
+            server.VirtualHost(network, "127.0.0.1:27000");
+
+            VirtualNetClient client = new VirtualNetClient();
+            client.VirtualConnect(network, server.VirtualAddress, "127.0.0.1:64332");
+            Thread.Sleep(2500);
+            client.Close(ShutdownType.NotifyAndWait);
+            Thread.Sleep(2500);
+            server.Close(ShutdownType.Immediate);
+        }
+    }
+}
