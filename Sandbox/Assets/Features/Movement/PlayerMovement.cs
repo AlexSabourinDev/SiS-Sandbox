e65fd4c629e19f8cf33442d98365d57b0dd0c422
Alex <AlexSabourinDev@gmail.com>
2019-08-19 20:27:42 -0400

    MOD: Create a simple 2D character movement script. This sho

--- Sandbox/Assets/Features/Movement/PlayerMovement.cs
+++ Sandbox/Assets/Features/Movement/PlayerMovement.cs
@@ -2,67 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float m_ForwardSpeed = 1.0f;
 
-    [SerializeField]
-    private float m_SideSpeed = 1.0f;
-
-    [SerializeField]
-    private float m_RotationSpeed = 1.0f;
-
-    private Rigidbody m_Rigidbody = null;
+    private Rigidbody2D m_Rigidbody = null;
 
     private void Awake()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        // Basic movement
-        {
-            float forward = 0.0f;
-            float right = 0.0f;
-            // Basic input, if we need something more complex, we'll add it then.
-            if(Input.GetKey(KeyCode.W))
-            {
-                forward = 1.0f;
-            }
-            else if(Input.GetKey(KeyCode.S))
-            {
-                forward = -1.0f;
-            }
-
-            if(Input.GetKey(KeyCode.D))
-            {
-                right = 1.0f;
-            }
-            else if(Input.GetKey(KeyCode.A))
-            {
-                right = -1.0f;
-            }
-
-            Vector3 forwardDir = transform.forward * forward;
-            Vector3 rightDir = transform.right * right;
-            Vector3 moveDir = Vector3.Normalize(forwardDir + rightDir);
-
-            float forwardMag = Mathf.Abs(Vector3.Dot(moveDir, forwardDir)) * m_ForwardSpeed;
-            float rightMag = Mathf.Abs(Vector3.Dot(moveDir, rightDir)) * m_SideSpeed;
-
-            float verticalVelocity = m_Rigidbody.velocity.y;
-            Vector3 moveVec = moveDir * (forwardMag + rightMag) + Vector3.up * verticalVelocity;
-
-            m_Rigidbody.velocity = moveVec;
-        }
-
-        // basic rotation
-        {
-            float horizontal = Input.GetAxis("Horizontal");
-            m_Rigidbody.angularVelocity = Vector3.up * horizontal * m_RotationSpeed;
-        }
+       if(Input.GetKey(KeyCode.D))
+       {
+           m_Rigidbody.velocity += Vector2.right * m_ForwardSpeed;
+       }
+       else if(Input.GetKey(KeyCode.A))
+       {
+           m_Rigidbody.velocity += Vector2.left * m_ForwardSpeed;
+       }
+       else
+       {
+           m_Rigidbody.velocity *= Vector2.up;
+       }
     }
 }
