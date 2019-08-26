aee7ba3bf3a1856003ecf7eb5ac3b893fc88717e
Alex <AlexSabourinDev@gmail.com>
2019-08-26 17:58:20 -0400

    ADD: Introduce test dialogue UI scene. It simply supports an input field and a scrollable test wwindow for testing. ADD: Test_DialogueUI.cs, a simply driver for the test dialogue scene. This class will simply pipe input to the dialogue system when we've decided what to do with it.

---
+++ Sandbox/Assets/Features/Dialogue/Test_DialogueUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_DialogueUI : MonoBehaviour
+{
+    public UnityEngine.UI.Text dialogueWindow;
+    public UnityEngine.UI.InputField inputField;
+
+    public void OnMessageInput(string message)
+    {
+        if(!string.IsNullOrWhiteSpace(message))
+        {
+            dialogueWindow.text += $"User: { message }\n";
+            inputField.text = string.Empty;
+            inputField.ActivateInputField();
+        }
+    }
+}
