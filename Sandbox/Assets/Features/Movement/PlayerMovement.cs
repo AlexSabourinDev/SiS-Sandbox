66f8cdb3bc74cd633286bf3ce7696bce38474e97
Alex <AlexSabourinDev@gmail.com>
2019-10-21 18:05:18 -0400

    MOD: Scrapped state machine movement. Went with something simple. Yay! TODO: Cucumbers

--- Sandbox/Assets/Features/Movement/PlayerMovement.cs
+++ Sandbox/Assets/Features/Movement/PlayerMovement.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Cat video log: https://youtu.be/rNSnfXl1ZjU?t=555
+// Note: Cuccumbers!
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
@@ -21,10 +24,13 @@ public class PlayerMovement : MonoBehaviour
     }
 
     [SerializeField]
-    private float m_MoveSpeed = 1.0f;
+    private float m_ForwardAcceleration = 5.0f;
 
     [SerializeField]
-    private float m_TurnTime = 1.0f;
+    private float m_TurnAcceleration = 10.0f;
+
+    [SerializeField]
+    private float m_MaxHorizontalVelocity = 10.0f;
 
     MovementState m_MovementState;
 
@@ -53,76 +59,31 @@ public class PlayerMovement : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        InputType inputType = ReadInput();
-        if(inputType == InputType.Jump)
+        float desiredDirection = 0.0f;
+
+        InputType input = ReadInput();
+        if(input == InputType.Right)
         {
-            // TODO: If falling:
-            //  - Freak out (TODO watch videos of cats falling)
-            //  - Jump if jump is pressed close enough to the ground
-            //  - Jump when falling might change the angle
+            desiredDirection = 1.0f;
+        }
+        else if(input == InputType.Left)
+        {
+            desiredDirection = -1.0f;
         }
 
-        // TODO:
-        //  - Movement left and right
-
-        m_MovementState = MovementState.Idle;
-
-        float currentVelocity = m_Rigidbody.velocity.x;
-        float nextVelocity = 0.0f;
-        if(inputType == InputType.Right)
+        float desiredAcceleration = m_ForwardAcceleration;
+        bool differentDirection = Mathf.Sign(m_Rigidbody.velocity.x) != desiredDirection;
+        if(differentDirection)
         {
-            nextVelocity = m_MoveSpeed;
-
-            float sign = Mathf.Sign(currentVelocity);
-            if(sign != 0.0f && sign == -1.0f) // Are we not going right?
-            {
-                m_MovementState = MovementState.Turn;
-            }
-            else
-            {
-                m_MovementState = MovementState.Move;
-            }
-        }
-        else if(inputType == InputType.Left)
-        {
-            nextVelocity = -m_MoveSpeed;
-
-            float sign = Mathf.Sign(currentVelocity);
-            if(sign != 0.0f && sign == 1.0f) // Are we not going right?
-            {
-                m_MovementState = MovementState.Turn;
-            }
-            else
-            {
-                m_MovementState = MovementState.Move;
-            }
+            desiredAcceleration = m_TurnAcceleration;
         }
 
-        if(m_MovementState == MovementState.Move)
+        if(Mathf.Abs(m_Rigidbody.velocity.x) < m_MaxHorizontalVelocity)
         {
-            m_Rigidbody.velocity = new Vector2(nextVelocity, m_Rigidbody.velocity.y);
-        }
-        else if(m_MovementState == MovementState.Turn)
-        {
-            float acceleration = m_MoveSpeed * 2.0f * Mathf.Sign(nextVelocity) / m_TurnTime;
-            m_Rigidbody.AddForce(Vector2.right * acceleration * m_Rigidbody.mass);
-
-            if(Mathf.Abs(m_Rigidbody.velocity.x) >= Mathf.Abs(nextVelocity))
-            {
-                m_MovementState = MovementState.Move;
-            }
-        }
-        else
-        {
-            float acceleration = m_MoveSpeed * -Mathf.Sign(currentVelocity) / m_TurnTime;
-            m_Rigidbody.AddForce(Vector2.right * acceleration * m_Rigidbody.mass);
-
-            if(Mathf.Abs(currentVelocity) < 0.01f)
-            {
-                m_Rigidbody.velocity = new Vector2(0.0f, m_Rigidbody.velocity.y);
-            }
+            Vector2 acceleration = Vector2.right * desiredAcceleration * desiredDirection;
+            m_Rigidbody.AddForce(acceleration * m_Rigidbody.mass);
         }
     }
 }
